dd1588b8a9ebfa62d89ce9af08138654da9c29c1
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-04-28 21:50:25 +0100

    Remove unnecessary divs

--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -7,17 +7,14 @@ function Navbar() {
   const [showLinks, setShowLinks] = useState(false);
   return (
     <div className='Navbar'>
-      <div className='leftSide'>
-        <div className='links' id={showLinks ? "hidden" : ""}>
-          <a href="/home">HOME</a>
-          <a href="/about">ABOUT</a>
-          <a href="/portfolio">PORTFOLIO</a>
-          <a href="/gallery">GALLERY</a>
-          <a href="/contact">CONTACT</a>
-        </div>
-        <button onClick={() => setShowLinks(!showLinks)}><ReorderIcon/></button>
+      <div className='links' id={showLinks ? "hidden" : ""}>
+        <a href="/home">HOME</a>
+        <a href="/about">ABOUT</a>
+        <a href="/portfolio">PORTFOLIO</a>
+        <a href="/gallery">GALLERY</a>
+        <a href="/contact">CONTACT</a>
       </div>
-      <div className='rightSide'></div>
+      <button onClick={() => setShowLinks(!showLinks)}><ReorderIcon/></button>
     </div>
   )
 }
