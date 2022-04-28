9c69349c2e98d7335ea69d8167c13cf079881f6c
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-04-28 21:43:39 +0100

    Capitalise menu items

--- src/components/Navbar/navBar.js
+++ src/components/Navbar/navBar.js
@@ -9,11 +9,11 @@ function Navbar() {
     <div className='Navbar'>
       <div className='leftSide'>
         <div className='links' id={showLinks ? "hidden" : ""}>
-          <a href="/home">Home</a>
-          <a href="/about">About</a>
-          <a href="/portfolio">Portfolio</a>
-          <a href="/gallery">Gallery</a>
-          <a href="/contact">Contact</a>
+          <a href="/home">HOME</a>
+          <a href="/about">ABOUT</a>
+          <a href="/portfolio">PORTFOLIO</a>
+          <a href="/gallery">GALLERY</a>
+          <a href="/contact">CONTACT</a>
         </div>
         <button onClick={() => setShowLinks(!showLinks)}><ReorderIcon/></button>
       </div>
