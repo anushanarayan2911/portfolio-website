a5a21e9e85895dcc1a8f610533ddc3d047469130
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-05-01 15:44:02 +0100

    Add example website

--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
-import {createTheme, ThemeProvider, makeStyles} from '@material-ui/core/styles';
-import Navbar from './components/Navbar/navBar'
-import Slideshow from './components/slideshow';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Pages/index'
+import React from 'react'
 
-const theme = createTheme({
-  palette : {
-    primary : {
-      main : '#fffffff'
-    },
-    secondary: {
-      main: '#000000'
-    }
-  },
-  typography : {
-    fontFamily : [
-      'runda'
-    ]
-  },
-  h4 : {
-    fontWeight: 300,
-    fontSize: 28,
-    lineHeight: '1rem'
-  },
-  h5 : {
-    fontWeight: 100,
-    lineHeight: '1rem'
-  }
-})
-
-const styles = makeStyles ({
-  wrapper: {
-    width: "65%",
-    margin: "auto",
-    textAlign: "center"
-  },
-  bigSpace: {
-    marginTop: "5rem"
-  },
-  littleSpace: {
-    marginTop: "2.5rem"
-  },
-  grid: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-})
 function App() {
-  const classes = styles();
   return (
-    <div className='App'>
-      <ThemeProvider theme={theme}>
-        <Navbar/>
-      </ThemeProvider>
-    </div>
-    
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,52 @@
+import { Card } from '@material-ui/core';
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'unlimited transactions with 0 fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../Images/1.svg').default,
+    alt: 'car',
+    dark: true,
+    primary: true,
+    darkText: false,
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer',
+    buttonLabel: 'Learn more',
+    imgStart: true,
+    img: require('../../Images/svg-2.svg').default,
+    alt: 'phone',
+    dark: false,
+    primary: true,
+    darkText: false,
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description: "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../Images/svg-3.svg').default,
+    alt: 'car',
+    dark: false,
+    primary: true,
+    darkText: false,
+}
