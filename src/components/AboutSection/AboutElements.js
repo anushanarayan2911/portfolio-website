24307de8b8490b41bf49e9a3f9b3982bef8fc941
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-05-03 14:23:37 +0100

    Add video to about page

--- src/components/AboutSection/AboutElements.js
+++ src/components/AboutSection/AboutElements.js
@@ -86,15 +86,12 @@ export const Img = styled.img`
 
 export const YoutubeEmbed = ({ embedId }) => (
     <div className='video-responsive'>
-        <iframe>
-            width = '853'
-            height = '480'
-            src = {`https://www.youtube.com/embed/ag9uYc9hJRk`}
-            frameBorder = '0'
-            allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-            allowFullScreen
-            title = 'EmbeddedYoutube'
-        </iframe>
+        <iframe
+        src="https://www.youtube.com/embed/ag9uYc9hJRk"
+        allow="autoplay; encrypted-media; fullscreen"
+        allowFullScreen
+        title="video"
+      />{" "}
     </div>
 )
 
