15b89a66fd71c14cca971e586d3332cffa66183b
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-05-08 18:29:44 +0100

    Finish ambio brace page

--- src/components/TheAmbioBrace/index.js
+++ src/components/TheAmbioBrace/index.js
@@ -24,6 +24,51 @@ const TheAmbioBrace = () => {
                 <Row>
                     <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-01.png')}/></Column>
                 </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-02.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-03.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-04.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-05.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-06.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-07.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-08.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-09.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-10.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-11.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-12.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-13.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-14.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-15.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/The Ambio Brace/Group 17_Documentation-16.png')}/></Column>
+                </Row>
             </Grid>
         </InfoWrapper>
     </InfoContainer>
