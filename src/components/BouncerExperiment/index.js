e5781c12930fca03c50dc325489f4c612b43a477
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-05-08 18:00:31 +0100

    Add all pages to bouncer experiment page

--- src/components/BouncerExperiment/index.js
+++ src/components/BouncerExperiment/index.js
@@ -24,6 +24,45 @@ const BouncerExperiment = () => {
                 <Row>
                     <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-01.png')}/></Column>
                 </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-02.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-03.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-04.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-05.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-06.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-07.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-08.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-09.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-10.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-11.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-12.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-13.png')}/></Column>
+                </Row>
+                <Row>
+                    <Column><Image src={require('../../Visuals/Portfolio/Bouncer Challenge/Anusha Narayan_Bouncer Challenge-14.png')}/></Column>
+                </Row>
             </Grid>
         </InfoWrapper>
     </InfoContainer>
