27b88de754f20fa1cc3d7a316becece4c6ba2d01
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-05-10 11:12:42 +0100

    Add description for social responsibility page

--- src/components/SocialResponsibility/index.js
+++ src/components/SocialResponsibility/index.js
@@ -3,6 +3,7 @@ import { InfoContainer,
     InfoWrapper,
     InfoRow, 
     TopLine,
+    Subtitle,
     Grid,
     Row,
     Column,
@@ -20,6 +21,12 @@ const SocialResponsibility = () => {
             <InfoRow>
                 <TopLine>Social Responsibility of Design Engineers</TopLine>
             </InfoRow>
+            <Subtitle>Duration: 2 weeks</Subtitle>
+            <br/>
+            <Subtitle>Date: December 2020</Subtitle>
+            <br/>
+            <Subtitle>Nature: an essay on the social responsbility of design engineers and culpability when their products cause harm, written in 1st year </Subtitle>
+            <br/>
             <Grid>
                 <Row>
                     <Column><Image src={require('../../Visuals/Portfolio/Social Responsibility of Design Engineers/NARAYAN_anusha-1.png')}/></Column>
