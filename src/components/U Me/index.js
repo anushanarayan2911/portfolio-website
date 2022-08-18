6ac96b874f483d84fe09b88e84c2399e4d45086e
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-08-18 17:53:07 +0100

    Add content to U Me Page

--- src/components/U Me/index.js
+++ src/components/U Me/index.js
@@ -53,11 +53,11 @@ const UMe = () => {
                 <PortfolioRow>
                     <Column>
                       <SectionHeading1>the problem</SectionHeading1>
-                      <Subtitle>Children with severe autism are not able to take part in many activities, as they are not able to communicate with anyone, including parents or carers. They are not able to communicate their needs or wants, and this can create frustration for both the carer and the child.</Subtitle>
+                      <Subtitle>Not being able to communicate their wants and needs is a daily struggle for children with severe autism. This causes frustration, as they struggle to communicate to even the people closest to them, such as parents or carers.</Subtitle>
                       <br/>
                     </Column>
                     <Column>
-                      <SectionHeading2>the outcomes</SectionHeading2>
+                      <SectionHeading2>the solution</SectionHeading2>
                       <Subtitle>The U Me is a two way communication device with an accompanying app. Parents/carers are able to set tasks using the app, whilst the child can use the device to ask for help</Subtitle>
                         <br/>
                         <br/>
@@ -67,8 +67,50 @@ const UMe = () => {
                 <br/>
                 <Row>
                     <PDFDiv>
-                        <Image src={require('../../Visuals/Portfolio/Arama/Anusha Concept Booklet-01.png')}/>
-                        
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-1.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-2.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-3.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-4.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-5.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-6.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-7.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-8.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-9.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-10.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-11.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-12.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-13.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-14.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-15.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-16.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-17.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-18.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-19.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-20.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-21.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-22.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-23.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-24.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-25.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-26.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-27.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-28.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-29.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-30.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-31.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-32.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-33.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-34.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-35.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-36.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-37.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-38.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-39.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-40.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-41.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-42.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-43.png')}/>
+                        <Image src={require('../../Visuals/Portfolio/U Me/Team 8 Phase 02-44.png')}/>
                     </PDFDiv>
                 </Row>
                 <br/>  
